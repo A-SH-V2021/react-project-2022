e03d6eee1161ba9dc2112d9b29735f9c14e95148
ali <narenje20142014@gmail.com>
2022-05-22 09:34:30 +0430

    'completenav'

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -10,6 +10,62 @@ function App() {
     <div className="App">
       <Topbar />
       <h1>admin panel</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, aliquid
+        sit perferendis expedita dolores nemo ab rerum. Cupiditate, quod
+        molestiae! Nobis porro sequi vel, nihil harum eum sapiente temporibus
+        sed? Dolores, illo ullam. Dolorum, molestiae rerum. Debitis in
+        voluptatum necessitatibus nulla exercitationem sunt tenetur ullam minus
+        cum est quos tempora nemo, mollitia suscipit vero sint accusamus. Autem
+        quam sint odio dolor? Aliquid cum maiores ratione velit voluptatem.
+        Voluptatibus non facilis exercitationem molestias eaque earum quisquam
+        dolor eum iste ipsum expedita incidunt, repudiandae accusantium deleniti
+        et aspernatur eius sint, eos, rerum soluta quidem voluptate id velit
+        eveniet? A quibusdam commodi ab facilis quos facere voluptate porro?
+        Placeat sit omnis neque sed quasi modi temporibus numquam eius quam
+        autem quibusdam officiis nobis perferendis, a facilis maiores voluptas
+        totam atque. Provident cumque quisquam omnis! Odit, ipsam quisquam.
+        Earum, ducimus incidunt eaque consequuntur nihil totam deleniti illum,
+        accusamus reiciendis minus sed? Fuga nam sunt labore? Aliquam
+        voluptatibus, officiis officia itaque quibusdam modi suscipit
+        consequuntur recusandae quo temporibus ducimus numquam accusamus vitae,
+        qui aperiam sapiente ex consectetur quidem inventore ipsa eius
+        necessitatibus molestias. Omnis veniam voluptatum eveniet expedita quo
+        maxime quia eius consequatur nostrum, ad illo obcaecati unde quidem
+        officia sequi explicabo vero? Ad vitae porro consectetur culpa.
+        Blanditiis voluptate omnis libero assumenda nulla in magnam amet
+        adipisci optio natus deleniti, quasi dolorem eos aspernatur alias?
+        Assumenda harum repellat pariatur itaque deleniti velit nihil commodi
+        maxime fugiat asperiores, officiis iusto. Sed blanditiis facilis
+        accusantium ex ipsum nostrum perspiciatis qui non velit. Magnam optio,
+        consectetur praesentium modi autem voluptatem totam unde quasi hic
+        ducimus? Architecto, praesentium deserunt. Fugit voluptatum veniam
+        excepturi ut aperiam aliquam! Repudiandae ducimus optio est nobis!
+        Voluptatibus praesentium tempore itaque quidem voluptate magnam aperiam
+        cumque distinctio adipisci incidunt ullam, dignissimos temporibus natus
+        impedit placeat illum aspernatur sit ea, cum fugiat. Laborum odit
+        distinctio fuga minus veniam quam architecto ducimus explicabo rem iste
+        quae et nobis adipisci ullam, modi facere amet culpa exercitationem sunt
+        itaque corporis? Libero aliquid amet, sequi magnam quibusdam aspernatur
+        reprehenderit, repellat dolorum illum cumque asperiores pariatur quae ad
+        architecto, qui commodi sed quidem placeat tempore enim error possimus?
+        Nesciunt ad vitae, recusandae autem alias possimus tempora accusantium
+        illo reiciendis id dolor aspernatur eum vel eligendi consequatur
+        temporibus sequi dignissimos quasi a soluta voluptatum molestias. Quia
+        dignissimos fugiat eos, aliquid facere quam aperiam sint, libero beatae
+        earum voluptatem cum quas mollitia at ipsum ipsam laboriosam recusandae
+        dolorum ratione omnis vel similique totam. Voluptates corporis animi
+        unde doloribus, itaque ullam fugiat quod similique nulla officia eum
+        saepe illo labore in porro iusto veritatis consequuntur sed eos non sit.
+        Nesciunt sapiente ut corporis ratione perferendis libero quidem deleniti
+        commodi vel error eos at maiores esse necessitatibus, atque nulla
+        adipisci molestiae aliquid nam! Quas, dolores architecto eveniet impedit
+        nisi, laborum optio repellat quae quos aspernatur nesciunt nemo amet
+        labore dolor rerum quaerat nam atque, tempora suscipit ipsa dolore a
+        fugiat et molestias. Similique minima laboriosam hic officiis provident
+        nobis dolore debitis, aliquam illum mollitia voluptate? Hic molestiae
+        ratione saepe laboriosam quidem, accusantium et expedita.
+      </p>
     </div>
   );
 }
--- admin-panel/src/Components/Topbar.js
+++ admin-panel/src/Components/Topbar.js
@@ -1,14 +1,25 @@
 import React from "react";
+import img from '../images/profile1.jpg'
 import styled from "styled-components";
-import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import {NotificationsActive,Settings,Language} from "@mui/icons-material";
 const Topbar = () => {
   return (
     <Wrapper>
       <div className="wrapper">
         <div className="logo">admin panel</div>
         <div className="right-section">
-          <NotificationsActiveIcon />
-          <span>1</span>
+          <div className="right-section-ele">
+            <NotificationsActive />
+            <span>1</span>
+          </div>
+          <div className="right-section-ele">
+            <Settings />
+            <span>1</span>
+          </div>
+          <div className="right-section-ele">
+            <Language />
+          </div>
+          <img src={img} alt="profile"  />
         </div>
       </div>
     </Wrapper>
@@ -21,6 +32,8 @@ const Wrapper = styled.nav`
   height: 5rem;
   background-color: #342170;
   color: white;
+  position: sticky;
+  top: 0;
   .wrapper {
     height: 100%;
     display: flex;
@@ -32,12 +45,15 @@ const Wrapper = styled.nav`
       cursor: pointer;
       font-size: 1.5rem;
     }
-    .right-section {
-      margin-right: 4rem;
+    .right-section{
+      display: flex;
+    }
+    .right-section-ele {
+      margin-right: 1rem;
       position: relative;
       cursor: pointer;
     }
-    .right-section > span {
+    .right-section-ele > span {
       position: absolute;
       top: -15px;
       right: -12px;
@@ -45,6 +61,15 @@ const Wrapper = styled.nav`
       width: 1.4rem;
       background-color: red;
       border-radius: 50%;
+    }
+    .right-section>img{
+      position: relative;
+      top: -4px;
+      margin-right: 1.5rem;
+      width: 2rem;
+      border-radius: 50%;
+      object-fit: cover;
+      cursor: pointer;
     }
   }
 `;
