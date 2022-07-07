690289c48b0fbf76c74f2e315e15d89e80e18f79
ali <narenje20142014@gmail.com>
2022-07-07 15:01:53 +0430

    'endOfCartshopping'

--- shopping-cart/src/components/ShoppingCartCanvas.tsx
+++ shopping-cart/src/components/ShoppingCartCanvas.tsx
@@ -1,3 +1,4 @@
+import data from "../data/items.json";
 import CartItemDetail from "../components/CartItemDetail";
 import {
   Offcanvas,
@@ -7,6 +8,7 @@ import {
   Stack,
 } from "react-bootstrap";
 import { useShoppingCart } from "../context/shoppingCartContext";
+import { formatCurrency } from "../utilities/formatCurrency";
 
 const ShoppingCartCanvas = () => {
   const { isOpenCart, closeCart, cartItems } = useShoppingCart();
@@ -21,6 +23,18 @@ const ShoppingCartCanvas = () => {
           {cartItems.map((item) => {
             return <CartItemDetail key={item.id} {...item}></CartItemDetail>;
           })}
+          <div className="ms-auto fs-3">
+            Total:{" "}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                console.log(total);
+
+                const item = data.find((i) => i.id === cartItem.id);
+                console.log(total);
+                return total + (item?.price || 0) * cartItem.quantity;
+              }, 0)
+            )}
+          </div>
         </Stack>
       </OffcanvasBody>
     </Offcanvas>
